4db897460156d13ad9a68d865276e7d69a769416
Shubham Singh Chouhan <shubhamsinghchouhan@live.com>
2026-01-01 23:30:44 +0530

    TodoApp Api Request methods and Routers

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -3,8 +3,14 @@
 from fastapi import FastAPI
 import models
 from database import engine
+from routers import auth, todos
 
 app = FastAPI()
 
 # Create new Database in FastApi
 models.Base.metadata.create_all(bind=engine)
+
+# routers
+# to include other python server files
+app.include_router(auth.router)
+app.include_router(todos.router)
