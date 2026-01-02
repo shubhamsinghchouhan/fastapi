59cf21d541795108c37d907c4a1e210018f4463d
Shubham Singh Chouhan <shubhamsinghchouhan@live.com>
2026-01-02 15:59:45 +0530

    Authenticate and autheorize

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,7 +1,62 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from starlette import status
+
+from database import SessionLocal
+from models import Users
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
-@router.get("/auth/")
-async def get_user():
-    return {"user": "authenticated"}
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
+
+@router.post("/auth/", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency,
+                      create_user_request: CreateUserRequest):
+    create_user_model = Users(
+            email=create_user_request.email,
+            username=create_user_request.username,
+            first_name=create_user_request.first_name,
+            last_name=create_user_request.last_name,
+            role=create_user_request.role,
+            hashed_password=bcrypt_context.hash(create_user_request.password),
+            is_active=True,
+        )
+    db.add(create_user_model)
+    db.commit()
+
+@router.post("/token")
+async def login_for_access_token(db: db_dependency, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = authenticate_user(username=form_data.username, password=form_data.password, db=db)
+    if not user:
+        return 'Failed to authenticate', status.HTTP_401_UNAUTHORIZED
+    return 'Success', status.HTTP_200_OK
