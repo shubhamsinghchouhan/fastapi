9025650c66fdf7ab5983208fcd20a4226ee9e18d
Shubham Singh Chouhan <shubhamsinghchouhan@live.com>
2026-01-04 00:42:21 +0530

    Added Alembic

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -5,9 +5,11 @@ from sqlalchemy.ext.declarative import declarative_base
 # /Users/shubhamsinghchouhan/projects/fastapi/TodoApp/database.py:11: MovedIn20Warning: The ``declarative_base()``
 # function is now available as sqlalchemy.orm.declarative_base(). (deprecated since: 2.0) (Background on SQLAlchemy 2.0 at: https://sqlalche.me/e/b8d9)
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost/TodoApplicationDatabase'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
