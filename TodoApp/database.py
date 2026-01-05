f41b620bbbc9592992899ff78665e04fcb7c304e
Shubham Singh Chouhan <shubhamsinghchouhan@live.com>
2026-01-05 16:41:09 +0530

    Init test

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
+# from sqlalchemy.ext.declarative import declarative_base
 # /Users/shubhamsinghchouhan/projects/fastapi/TodoApp/database.py:11: MovedIn20Warning: The ``declarative_base()``
 # function is now available as sqlalchemy.orm.declarative_base(). (deprecated since: 2.0) (Background on SQLAlchemy 2.0 at: https://sqlalche.me/e/b8d9)
 
